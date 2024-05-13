8110a6fe83f17b71553e5d0b65240a367888487e
geraldAinomugisha19 <ainomugishagerald19@gmail.com>
2024-05-13 07:22:35 -0700

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,26 @@
+<?php
+$conn = mysqli_connect("localhost","root","","requirement");
+
+if(!conn){
+    echo"failed to connect to the database";
+}else{
+    echo"connected to the database";
+}
+?>
+
+
+
+
+<!DOCTYPE html>
+<htmml>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport">
+        content="
+        width=device-width,
+        initial-scale=1">
+        <title>connect to your database</title>
+</head>
+<body>
+</body>
+</html>
